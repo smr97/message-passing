f4882479bac59d92cdc476f6d28374285fc05577
smr97 <saurabh.mraje@gmail.com>
2016-04-14 22:17:00 +0530

    Update message.c
    Working prototype of a mailbox system using 2 threads and one queue. Gives error on receive for more than 6 characters

--- message.c
+++ message.c
@@ -4,22 +4,24 @@
 #include <sys/stat.h>
 #include <pthread.h>
 #include <errno.h> 
-
+#include <stdlib.h>
+#include <string.h>
 pthread_t t1, t2;
 pthread_mutex_t m1;
 pthread_cond_t c1;
+pthread_barrier_t b;
 
 mqd_t global;
 
 createQ()
 {
 	struct mq_attr buf;        /* buffer for stat info */
-    buf.mq_msgsize = 10;
+    buf.mq_msgsize = 200000;
     buf.mq_maxmsg = 20;
     
-    int flag = O_RDWR|O_NONBLOCK;
+    int flag = O_RDWR|O_NONBLOCK;//|O_CREAT;
     
-	global = mq_open("/chut", flag, 0777, &buf);
+	global = mq_open("/test", flag, 0777, &buf);
 	
 	printf("queue opened\n");
 	
@@ -39,13 +41,16 @@ void *f1(void *arg)
 	
 	printf("t1 opened q\n");
 	
-	if(!mq_send(global, "1", 1, 0))
+	if(!mq_send(global, "abcdefg", 7, 0))
 	perror("mq_send()");
 	
 	mq_close(global);
 	//mq_unlink("/chut");
 	printf("message sent\n");
 	pthread_mutex_unlock(&m1);
+	
+	pthread_barrier_wait(&b);
+	
 	pthread_cond_signal(&c1);
 	
 	
@@ -54,37 +59,43 @@ void *f1(void *arg)
 
 void *f2(void *arg)
 {
-	char msg[50];
+	char *msg;
+	msg = (char *) malloc(250);
 	struct mq_attr at;	
 	int pri;
 	printf("t2 spawned\n");
 	
 	pthread_cond_wait(&c1, &m1);
+	
+	
 	//pthread_mutex_lock(&m1);
+	
 	
 	printf("t2 got mutex\n");
 	
 	createQ();
 	
-	int ret = mq_receive(global, msg, 50, &pri); 
+	int ret = mq_receive(global, msg, 20, &pri); 
 	if(ret<0)
 	{perror("mq_receive()");}
 	
-	//mq_close(global);
+	mq_close(global);
 	//mq_unlink("/chut");
 	pthread_mutex_unlock(&m1);
 	
 	printf("%s\n", msg);
 	
-	
+	free(msg);
 }
 
 int main()
 {
 	pthread_mutex_init(&m1, 0);
     pthread_cond_init(&c1, 0);
+    pthread_barrier_init(&b, 0, 2);
     pthread_create(&t1, 0, f1, NULL);
     pthread_create(&t2, 0, f2, NULL);
+    pthread_barrier_wait(&b);
     pthread_join(t1, NULL);
     pthread_join(t2, NULL);
     
